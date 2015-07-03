cb7e597501826e42e51dd2792c85c5bbb15618d3
g.demarco <devnull@localhost>
2015-07-03 09:57:47 +0200

    bugfix

--- src/java/it/bologna/ausl/bds_tools/downloader/Downloader.java
+++ src/java/it/bologna/ausl/bds_tools/downloader/Downloader.java
@@ -81,7 +81,8 @@ public class Downloader extends HttpServlet {
             //String plugin = parts[0];
             String plugin = (String) downloadParams.get("plugin");
             //String server = parts[1];
-            String server = (String) downloadParams.get("server");
+//            String server = (String) downloadParams.get("server");
+            String server = ApplicationParams.getServerId();
             String connParameters = getServletContext().getInitParameter(server);
             if (connParameters == null) {
                 throw new ServletException(server + " not found in parameters");
@@ -119,10 +120,6 @@ public class Downloader extends HttpServlet {
         IOUtils.closeQuietly(in);
     }
 
-    /**
-     * @see HttpServlet#doPost
-     * @param request(HttpServletRequest request, HttpServletResponse response)
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // TODO Auto-generated method stub
