eaac53f6ba95e118a94c0ea4339cce9a9075fa50
a.marcomini <devnull@localhost>
2015-09-03 17:13:36 +0200

    modifica IodaFascicoli

--- src/java/it/bologna/ausl/bds_tools/ioda/utils/IodaFascicoliUtilities.java
+++ src/java/it/bologna/ausl/bds_tools/ioda/utils/IodaFascicoliUtilities.java
@@ -161,6 +161,9 @@ public class IodaFascicoliUtilities {
             res.addFascicolo(f);
         }
         
+        if(res.getSize() == 0){
+            res.createFascicoli();
+        }
                
         return res;
     }
