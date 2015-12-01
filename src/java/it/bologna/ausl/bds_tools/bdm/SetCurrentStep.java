842df474b6d395fb9e8fc7dd66d16b99af0e8c1d
g.demarco <devnull@localhost>
2015-12-01 12:47:51 +0100

    refactoring path della servlet SetCurrentStep

--- src/java/it/bologna/ausl/bds_tools/bdm/SetCurrentStep.java
+++ src/java/it/bologna/ausl/bds_tools/bdm/SetCurrentStep.java
@@ -1,11 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package it.bologna.ausl.bds_tools.bdm;
 
-import it.bologna.ausl.bds_tools.SetCurrentActivity;
 import it.bologna.ausl.bds_tools.exceptions.NotAuthorizedException;
 import it.bologna.ausl.bds_tools.utils.UtilityFunctions;
 import java.io.IOException;
