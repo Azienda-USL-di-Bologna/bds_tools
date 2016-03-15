0b92be1bff721607e33d6acc84def2d0d7065fe9
a.marcomini <devnull@localhost>
2016-03-15 15:43:35 +0100

    aggiustata query temporanea getFascicolazione

--- src/java/it/bologna/ausl/bds_tools/ioda/utils/IodaFascicolazioniUtilities.java
+++ src/java/it/bologna/ausl/bds_tools/ioda/utils/IodaFascicolazioniUtilities.java
@@ -106,7 +106,8 @@ public class IodaFascicolazioniUtilities {
                     "FROM " + getGdDocTable() + " g, " + getFascicoliGdDocTable() + " fg  " +
                     "WHERE g.id_oggetto_origine = ? " +
                     "AND g.tipo_oggetto_origine = ? " +
-                    "AND g.id_gddoc = fg.id_gddoc";
+                    "AND g.id_gddoc = fg.id_gddoc " + 
+                    "AND fg.data_eliminazione IS NULL ";
 
         ps = dbConn.prepareStatement(sqlText);
         int index = 1;
@@ -205,7 +206,8 @@ public class IodaFascicolazioniUtilities {
                 "AND g.tipo_oggetto_origine = ? " +
                 "AND g.id_gddoc = fg.id_gddoc " +
                 "AND f.id_fascicolo = fg.id_fascicolo " +
-                "AND fg.id_fascicolo = ? ";
+                "AND fg.id_fascicolo = ? " + 
+                "AND fg.data_eliminazione IS NULL ";
         
         ps = dbConn.prepareStatement(sqlText);
         ps.setString(1, sd.getIdOggettoOrigine());
